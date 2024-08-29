e1830f67616d66e9b447ba09ea8da64c383a9aba
SudhakrJha <140058829+SudhakrJha@users.noreply.github.com>
2024-08-29 14:31:32 +0000

    added HTML CSS

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def hello_world():
-  return 'Hello, Sudhakar'
+  return render_template('home.html')
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
