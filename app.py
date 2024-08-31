e068f3fde8270514efe5add64a54b64a2351a645
SudhakrJha <140058829+SudhakrJha@users.noreply.github.com>
2024-08-31 17:57:11 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -1,12 +1,66 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'role': 'Data Analyst',
+    'location': 'Bengaluru',
+}, {
+    'id': 2,
+    'role': 'Data Scientist',
+    'location': 'Noida',
+    'Salary': '12k'
+}, {
+    'id': 3,
+    'role': 'Backend Developer',
+    'location': 'Hyderabad',
+    'Salary': '9k'
+}]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, name="Sudhakar")
+
+
+@app.route('/jobs')
+def jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'role': 'Data Analyst',
+    'location': 'Bengaluru',
+}, {
+    'id': 2,
+    'role': 'Data Scientist',
+    'location': 'Noida',
+    'Salary': '12k'
+}, {
+    'id': 3,
+    'role': 'Backend Developer',
+    'location': 'Hyderabad',
+    'Salary': '9k'
+}]
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html', jobs=JOBS, name="Sudhakar")
+
+
+@app.route('/api/jobs')
+def jobs():
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
